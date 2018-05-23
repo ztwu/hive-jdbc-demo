0c8ac74a65fa04b34de561facf064326c701614c
ztwu <123>
2018-05-23 16:02:37 +0800

    优化

---
+++ src/main/java/com/iflytek/edu/HiveJDBCTest.java
@@ -0,0 +1,61 @@
+package com.iflytek.edu;
+
+import java.sql.*;
+
+/**
+ * Created with Intellij IDEA.
+ * User: ztwu2
+ * Date: 2017/8/14
+ * Time: 11:29
+ * Description
+ */
+
+public class HiveJDBCTest {
+    private static String driverName =
+            "org.apache.hive.jdbc.HiveDriver";
+
+    public static void main(String[] args)
+            throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        Connection con = DriverManager.getConnection(
+                "jdbc:hive2://192.168.1.100:10000/ztwu", "root", "");
+        Statement stmt = con.createStatement();
+        String tableName = "user_source";
+
+        // show tables
+        String sql = "show tables '" + tableName + "'";
+        System.out.println("Running: " + sql);
+        ResultSet res = stmt.executeQuery(sql);
+        if (res.next()) {
+            System.out.println(res.getString(1));
+        }
+
+        // describe table
+        sql = "describe " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+
+        sql = "select * from " + tableName;
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(String.valueOf(res.getInt(1)) + "\t"
+                    + res.getString(2));
+        }
+
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+    }
+}
